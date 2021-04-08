731daf95a75eb792ef77d8086d6ef7415dd754de
Marcela00 <lina.tic.isc@gmail.com>
2021-04-08 00:38:13 -0500

    This first commit will have the basic operations

---
+++ src/com/company/OperacionesBasicas.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class OperacionesBasicas {
+    Int number1;
+    int number2;
+    double resultado;
+
+    //constructor
+    public OperacionesBasicas(){
+
+        number1=0;
+        number2=0;
+        resultado=0;
+    }
+    //métodos que se usarán
+    public void suma(){
+
+    }
+
+    public void resta(){
+
+    }
+
+}
